2300333416005bf311e17867bb2064ca12ac476f
TranHongNhien04 <hongnhientran.doylely@gmail.com>
2025-04-10 15:24:48 +0700

    2. get api Overview

--- tuan08_06-04/labtuan08/src/components/Overview.jsx
+++ tuan08_06-04/labtuan08/src/components/Overview.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Squares from '../assets/img/Squares four 1.png';
 import File_text_1 from '../assets/img/File text 1.png';
 import ImportIcon from '../assets/img/Move up.png';
@@ -9,6 +10,41 @@ import Button1530 from '../assets/img/Button 1530.png';
 import create from '../assets/img/create.png';
 
 function Overview() {
+    const [data, setData] = useState({
+        turnover: null,
+        profit: null,
+        newCustomer: null,
+    });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const [turnoverRes, profitRes, newCustomerRes, ordersRes] =
+                    await Promise.all([
+                        axios.get('http://localhost:3000/Turnover'),
+                        axios.get('http://localhost:3000/Profit'),
+                        axios.get('http://localhost:3000/New_customer'),
+                    ]);
+
+                const turnover = turnoverRes?.data ?? null;
+                const profit = profitRes?.data ?? null;
+                const newCustomer = newCustomerRes?.data ?? null;
+
+                setData({ turnover, profit, newCustomer });
+
+
+                setLoading(false);
+            } catch (err) {
+                console.error('Lỗi xảy ra khi gọi API:', err);
+                setError(err.message || 'Đã xảy ra lỗi');
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
     return (
         <>
             <div className="col-span-3 row-span-3 grid grid-cols-3 grid-rows-3 gap-x-3 border-b p-4">
@@ -17,34 +53,122 @@ function Overview() {
                     <span>Overview</span>
                 </div>
                 <div className="col-span-1 row-span-2 bg-pink-100 rounded-md p-3">
-                    <div className="flex justify-between">
-                        <h3 className="text-sm font-bold">Turnover</h3>
-                        <button>
-                            <img src={Button1509} alt="" className="h-8 w-8" />
-                        </button>
-                    </div>
-                    <div>$92,405</div>
-                    <div>^5.39%</div>
+                    {data.turnover ? (
+                        <>
+                            <div className=" flex justify-between">
+                                <h3 className="text-sm font-bold">
+                                    Turnover
+                                </h3>
+
+                                <button>
+                                    <img
+                                        src={Button1509}
+                                        alt=""
+                                        srcset=""
+                                        className="h-8 w-8"
+                                    />
+                                </button>
+                            </div>
+
+                            <p className="text-2xl font-bold">
+                                $
+                                {new Intl.NumberFormat().format(
+                                    data.turnover.amount
+                                )}{' '}
+                            </p>
+
+                            <p
+                                className={`${data.turnover.changeDirection === 'up'
+                                    ? 'text-green-700'
+                                    : 'text-red-600'
+                                    } text-sm`}>
+                                {data.turnover.changeDirection === 'up'
+                                    ? '▲'
+                                    : '▼'}{' '}
+                                {data.turnover.changePercentage} period of
+                                change
+                            </p>
+                        </>
+                    ) : (
+                        <div>No data available</div>
+                    )}
                 </div>
                 <div className="col-span-1 row-span-2 bg-blue-100 rounded-md p-3">
-                    <div className="flex justify-between">
-                        <h3 className="text-sm font-bold">Turnover</h3>
-                        <button>
-                            <img src={Button1529} alt="" className="h-8 w-8" />
-                        </button>
-                    </div>
-                    <div>$92,405</div>
-                    <div>^5.39%</div>
+                    {data.profit ? (
+                        <>
+                            <div className=" flex justify-between">
+                                <h3 className="text-sm font-bold">
+                                    Profit
+                                </h3>
+
+                                <button>
+                                    <img
+                                        src={Button1529}
+                                        alt=""
+                                        srcset=""
+                                        className="h-8 w-8"
+                                    />
+                                </button>
+                            </div>
+
+                            <p className="text-2xl font-bold">
+                                $
+                                {new Intl.NumberFormat().format(
+                                    data.profit.amount
+                                )}{' '}
+                            </p>
+                            <p
+                                className={`${data.profit.changeDirection === 'up'
+                                    ? 'text-green-700'
+                                    : 'text-red-600'
+                                    } text-sm`}>
+                                {data.profit.changeDirection === 'up'
+                                    ? '▲'
+                                    : '▼'}{' '}
+                                {data.profit.changePercentage} period of change
+                            </p>
+                        </>
+                    ) : (
+                        <div>No data available</div>
+                    )}
                 </div>
                 <div className="col-span-1 row-span-2 bg-blue-100 rounded-md p-3">
-                    <div className="flex justify-between">
-                        <h3 className="text-sm font-bold">Turnover</h3>
-                        <button>
-                            <img src={Button1530} alt="" className="h-8 w-8" />
-                        </button>
-                    </div>
-                    <div>$92,405</div>
-                    <div>^5.39%</div>
+                    {data.newCustomer ? (
+                        <>
+                            <div className=" flex justify-between">
+                                <h3 className="text-sm font-bold">
+                                    New Customer
+                                </h3>
+
+                                <button>
+                                    <img
+                                        src={Button1530}
+                                        alt=""
+                                        srcset=""
+                                        className="h-8 w-8"
+                                    />
+                                </button>
+                            </div>
+
+                            <p className="text-2xl font-bold">
+                                {data.newCustomer.count}{' '}
+                            </p>
+
+                            <p
+                                className={`${data.newCustomer.changeDirection === 'up'
+                                    ? 'text-green-700'
+                                    : 'text-red-600'
+                                    } text-sm`}>
+                                {data.newCustomer.changeDirection === 'up'
+                                    ? '▲'
+                                    : '▼'}{' '}
+                                {data.newCustomer.changePercentage} period of
+                                change
+                            </p>
+                        </>
+                    ) : (
+                        <div>No data available</div>
+                    )}
                 </div>
             </div>
             <div className="col-span-3 row-span-6 border-b p-4">
